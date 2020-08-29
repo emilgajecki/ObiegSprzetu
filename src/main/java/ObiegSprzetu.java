274dddf2f04f154c7f2c008b74dfe8b543e940c2
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-08-29 10:57:14 +0200

    kolory w stylach css

--- src/main/java/ObiegSprzetu.java
+++ src/main/java/ObiegSprzetu.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,12 +12,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class ObiegSprzetu extends Application {
+public class ObiegSprzetu extends Application implements EventHandler<ActionEvent> {
 
     //manager kompozycji
     private GridPane gridPane;
     // tytul naszego formularza - nagłówek
-    private Text fromTitle, powiadomieniePrzycisku;
+    private Text fromTitle, notification; // powiadomieniePrzycisku
     //etykiety
     private Label userLoginLabel, userPasswordLabel;
     // pole do loginu i hasła
@@ -44,6 +46,7 @@ public class ObiegSprzetu extends Application {
         gridPane.setGridLinesVisible(false);
 
         fromTitle = new Text("Witam");
+        fromTitle.setId("formTitle");
         // elementy dodwane do siatki - pierwszy paramet to taki, ktory chcemy dodac
         gridPane.add(fromTitle, 0, 0, 2, 1);
         // dodajemy etykiete logowania
@@ -52,16 +55,24 @@ public class ObiegSprzetu extends Application {
         userLoginField = new TextField();
         gridPane.add(userLoginField, 1, 1);
 
-       userPasswordLabel = new Label("user password:");
-       gridPane.add(userPasswordLabel, 0, 2);
-       userPasswordField = new PasswordField();
-       gridPane.add(userPasswordField,1,2);
+        userPasswordLabel = new Label("user password:");
+        gridPane.add(userPasswordLabel, 0, 2);
+        userPasswordField = new PasswordField();
+        gridPane.add(userPasswordField,1,2);
 
         singInButton = new Button("Zaloguj");
+        singInButton.setOnAction(this);
         gridPane.add(singInButton, 1, 4);
 
+        notification = new Text();
+        notification.setId("notification");
+        gridPane.add(notification, 1,6);
 
-        scene = new Scene(gridPane, 800, 800);
+        // dodanie css do wygladu - sciezka
+        cssPath = this.getClass().getResource("applications.css").toExternalForm();
+        scene = new Scene(gridPane, 600, 600);
+        // dodanie do sceny
+        scene.getStylesheets().add(cssPath);
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setTitle("Obieg Sprzetu"); //nadajemy tytul okna
@@ -69,5 +80,15 @@ public class ObiegSprzetu extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    // obsluga akciji na przycisku
+    @Override
+    public void handle(ActionEvent event) {
+
+        if(event.getSource()==singInButton){
+            notification.setText("Zalogowano poprawnie");
+        }
+
     }
 }
