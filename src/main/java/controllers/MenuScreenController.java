dad8dca9406aff4a38d03aaf8696c67a5587288c
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-09-10 20:48:01 +0200

    edycja okna glownego - bede wracał przyciskiem wstecz do tego okna

--- src/main/java/controllers/MenuScreenController.java
+++ src/main/java/controllers/MenuScreenController.java
@@ -16,17 +16,53 @@ public class MenuScreenController {
     }
 
     @FXML
-    public void openApplication(){
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/App.fxml"));
-        Pane pane =null ;
+    public void closeApp() {
+        Platform.exit();
+    }
+
+    @FXML
+    public void newUser() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/NewUser.fxml"));
+        Pane pane = null;
         try {
             pane = loader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        AppController appController = loader.getController();
-        appController.setMainControllers(mainControllers);
+        // wszystkie okna do mainControllera
         mainControllers.setScreen(pane);
+    }
+
+    @FXML
+    public void newDevice() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/NewDevice.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainControllers.setScreen(pane);
+    }
+
+    @FXML
+    public void seeUser() {
+
+    }
+
+    @FXML
+    public void seeDevice() {
+
+    }
+
+    @FXML
+    public void checkUser() {
+
+    }
+
+    @FXML
+    public void checkDevice() {
+
     }
 
 }
