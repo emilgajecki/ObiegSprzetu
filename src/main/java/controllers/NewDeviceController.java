efab24f242d2cadd7d796c0781f781b44942f22c
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-09-13 11:30:52 +0200

    dodanie funkcji menu

--- src/main/java/controllers/NewDeviceController.java
+++ src/main/java/controllers/NewDeviceController.java
@@ -1,9 +1,11 @@
 package controllers;
 
-
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import java.awt.*;
+import java.io.IOException;
 
 public class NewDeviceController {
 
@@ -11,9 +13,73 @@ public class NewDeviceController {
     private TextField deviceName;
     private TextField deviceModel;
 
+    @FXML
+    public void newUser() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/NewUser.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        NewUserController newUserController = loader.getController();
+        newUserController.setMainControllers(mainControllers);
+        // wszystkie okna do mainControllera
+        mainControllers.setScreen(pane);
+    }
+
+    @FXML
+    public void newDevice() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/NewDevice.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        NewDeviceController newDeviceController = loader.getController();
+        newDeviceController.setMainControllers(mainControllers);
+        mainControllers.setScreen(pane);
+    }
+
+    @FXML
+    public void seeUser() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/SeeUser.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SeeUserController seeUserController = loader.getController();
+        seeUserController.setMainControllers(mainControllers);
+        mainControllers.setScreen(pane);
+
+    }
+
+    @FXML
+    public void seeDevice() {
+
+    }
+
+    @FXML
+    public void checkUser() {
+
+    }
+
+    @FXML
+    public void checkDevice() {
+
+    }
+    @FXML
+    public void closeApp() {
+        Platform.exit();
+    }
+
     public void setMainControllers(MainControllers mainControllers) {
         this.mainControllers = mainControllers;
     }
+
     @FXML
     public TextField getDeviceName() {
         return deviceName;
@@ -25,16 +91,7 @@ public class NewDeviceController {
     }
 
     @FXML
-    public void backMenu(){
-        mainControllers.loadMenuScreen();
-    }
-
-    @FXML
-    public void addDeviceToDatabase(){
+    public void addDeviceToDatabase() {
 
     }
-
-
-
-
 }
